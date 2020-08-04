711bc73fc1571cc5630f4c922217e457ba837c3e
ElistratovNik <elistratovnik@yandex.ru>
2020-08-04 13:54:26 +0300

    add adaptive

--- src/about/index.js
+++ src/about/index.js
@@ -1,1 +1,37 @@
+import 'swiper/swiper-bundle.css';
 import "../pages/about.css";
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+Swiper.use([Navigation, Pagination]);
+var mySwiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 40,
+  breakpoints: {
+    680: {
+      spaceBetween: 8,
+    },
+
+    768: {
+      slidesPerView: 2,
+      loop: false,
+      spaceBetween: 8,
+      centeredSlides: false,
+    },
+
+    1000: {
+      slidesPerView: 3,
+      centeredSlides: true,
+      spaceBetween: 16,
+    }
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
